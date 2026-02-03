7effa84a750d85111d91ea0ac10983ae412ba47e
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2026-02-03 23:40:31 +0100

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center bg-gradient-to-br from-blue-500 via-blue-400 to-yellow-300">
+      <h1
+        className="text-center font-extrabold text-5xl md:text-7xl lg:text-8xl tracking-wide
+                     text-yellow-300 drop-shadow-[0_6px_0_#1e40af]"
+      >
+        Onchain Mood NFT
+      </h1>
+    </div>
+  );
+}
+
+export default App;
